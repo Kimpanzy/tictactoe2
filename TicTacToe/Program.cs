afb52053413eb8c15ff0e6d70fd5a3facc090238
Kimpanzy <32914744+Kimpanzy@users.noreply.github.com>
2022-11-30 13:46:25 +0100

    Add files via upload

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,20 @@
+﻿//1. Provide instruction
+//1.1 A Greeting
+//1.2 Display player sign, Player 1 is X, Player 2 is O
+//1.3 Who's turn is it
+//1.4 Instruct player to enter 1-9
+//2. Draw game board
+//2.1 Game will have a board with 3 rows and 3 colums with 1-9
+//3. As the user places markers on the game board then notifying which player turn
+//3.1 After each turn judge if there is a winner
+//3.2 If no winner keep playing by going back to step 1
+//3.3 If all markers are placed and no winner its a draw
+//3.4 If we have a winner, announce who won and stop the game
+
+Console.WriteLine("+___+___+___+");
+Console.WriteLine("| 1 | 2 | 3 |");
+Console.WriteLine("+___+___+___+");
+Console.WriteLine("| 4 | 5 | 6 |");
+Console.WriteLine("+___+___+___+");
+Console.WriteLine("| 7 | 8 | 9 |");
+Console.WriteLine("+___+___+___+");
